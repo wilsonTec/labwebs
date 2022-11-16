6f9f226dc646fda67a9f9962dbe7433b5bfd6152
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-11-16 18:27:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'laboratorio12';
+  paises: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get("https://api.covid19api.com/summary")
+      .subscribe(
+        resultado => {
+          this.paises = resultado;
+        }
+      );
+  }
+
 }
+
